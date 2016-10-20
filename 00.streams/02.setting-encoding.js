cd7866cb24c5bbe05f2195553575d384deeee8e5
Dexter Nguyen <jjm.nvl@gmail.com>
2016-10-20 16:47:03 +0700

    NodeJs stream - setting encoding

--- 00.streams/02.setting-encoding.js
+++ 00.streams/02.setting-encoding.js
@@ -1,10 +1,25 @@
 (function () {
     'use strict';
 
+    const fs = require('fs');
+    var readableStream = fs.createReadStream('./test_data/file.txt');
+    var data = '';
 
+    readableStream.setEncoding('utf8');
+
+    readableStream.on('data', (chunk) => {
+        console.log(typeof chunk);
+
+        data += chunk;
+    });
+
+    readableStream.on('end', () => {
+        console.log(data);
+    });
 }());
 
 /**
  * By default the data you read from a stream is a "Buffer" object. If you are reading strings
- * this may not be suitable for you.
+ * this may not be suitable for you. So, you can set encoding on the stream by "Readable.setEncoding()"
+ * as show above.
  */
